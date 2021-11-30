fdece4202f64e79ebe7bbc70d8566edafbff81c7
John Merz <jmerz826@gmail.com>
2021-11-30 17:15:58 -0500

    user card built out in App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ class App extends React.Component {
     user: ''
   }
 
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/jmerz826`)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          avatar: res.data.avatar_url,
+          name: res.data.name,
+          username: res.data.login,
+          repos: res.data.public_repos,
+          followerCount: res.data.followers,
+        })
+      })
+  }
+
   handleChange = (e) => {
     this.setState({
       ...this.state,
@@ -19,8 +33,18 @@ class App extends React.Component {
     axios.get(`https://api.github.com/users/${this.state.user}`)
       .then(res => {
         console.log(res.data);
+        this.setState({
+          ...this.state,
+          user: '',
+          username: res.data.login,
+          name: res.data.name,
+          repos: res.data.public_repos,
+          followerCount: res.data.followers,
+          avatar: res.data.avatar_url
+        })
       })
-      .catch(err => console.error(err));
+      .catch(err => console.error(err))
+    ;
   }
 
   render() {
@@ -31,12 +55,17 @@ class App extends React.Component {
           <input
             type='text'
             placeholder='GitHub Handle'
+            value={this.state.user}
             onChange={this.handleChange}
           />
           <button onClick={this.handleSearch}>Search</button>
         </form>
-        <div>
-
+        <div id='user-card'>
+          <img src={this.state.avatar} />
+          <h2>{this.state.name}</h2>
+          <h3>{this.state.username}</h3>
+          <h3>Total Repos: {this.state.repos}</h3>
+          <h3>Total Followers: { this.state.followerCount}</h3>
         </div>
       </div>
     );
